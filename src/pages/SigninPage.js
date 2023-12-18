38198ae8d6e38dfe80bc2d4426e787345afc2320
aldinhalilovic <aldinhalilovic06@gmail.com>
2023-12-18 18:14:55 +0100

    setup

--- src/pages/SigninPage.js
+++ src/pages/SigninPage.js
@@ -1,7 +1,16 @@
 import React from "react";
 
 function SigninPage() {
-  return <div>SigninPage</div>;
+  return (
+    <div>
+      {/* <div></div>  50% width*/}
+      {/* <img />  50% width */}
+    </div>
+  );
 }
 
 export default SigninPage;
+
+// https://buttercms.com/blog/building-reusable-components-using-react/
+
+// <Button color="red" onPress={() => console.log("nesto")} />

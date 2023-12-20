4e5486d6a144c17356720343f258c28f2db05cfd
aminnicevic04 <aminnicevic@gmail.com>
2023-12-20 17:26:53 +0100

    update

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,7 +1,26 @@
 import React from "react";
+import "./HomePage.css";
 
 function HomePage() {
-  return <div>HomePage</div>;
+  return (
+    <div className="main-cont">
+      <div className="nav-cont">
+        <h1>Home</h1>
+        <h1>Logo</h1>
+        <h1>Contact</h1>
+      </div>
+      <div className="content-cont">
+        <div className="content"></div>
+      </div>
+      <div className="text">
+        <h1 id="h1">Save our forest</h1>
+        <p id="p">
+          Committed to preserving nature, we safeguard forests as precious
+          resources for a sustainable future, providing the care they need.
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default HomePage;

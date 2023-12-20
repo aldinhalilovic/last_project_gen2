469b9a445f96d6a385687a19e14a9206fb2ad88f
aldinhalilovic <aldinhalilovic06@gmail.com>
2023-12-20 18:11:14 +0100

    contact form created and implented in contactusPage

--- src/pages/ContactusPage.js
+++ src/pages/ContactusPage.js
@@ -1,7 +1,20 @@
 import React from "react";
+import ContactForm from "../containers/Contact/ContactForm";
 
 function ContactusPage() {
-  return <div>ContactusPage</div>;
+  return (
+    <div className="main-cont">
+      <div className="nav-cont">
+        <h1>Home</h1>
+        <h1>Logo</h1>
+        <h1>Contact</h1>
+      </div>
+      <div className="content-cont">
+        <div className="content"></div>
+        <ContactForm />
+      </div>
+    </div>
+  );
 }
 
 export default ContactusPage;
